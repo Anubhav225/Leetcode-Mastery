eb313339259547328ce4bfa07febc46f6ebf31bd
Anubhav225 <anubhavdas11c05@gmail.com>
2026-09-19 22:53:43 +0530

    Time: 24 ms (79.77%), Space: 29.5 MB (60.88%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.py
@@ -0,0 +1,54 @@
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
+
+class LRUCache:
+
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.cache = {}
+
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def remove(self, node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
+
+    def add(self, node):
+        node.prev = self.head
+        node.next = self.head.next
+
+        self.head.next.prev = node
+        self.head.next = node
+
+    def get(self, key):
+        if key not in self.cache:
+            return -1
+
+        node = self.cache[key]
+
+        self.remove(node)
+        self.add(node)
+
+        return node.value
+
+    def put(self, key, value):
+        if key in self.cache:
+            self.remove(self.cache[key])
+
+        node = Node(key, value)
+        self.cache[key] = node
+        self.add(node)
+
+        if len(self.cache) > self.capacity:
+            node = self.tail.prev
+            self.remove(node)
+            del self.cache[node.key]
